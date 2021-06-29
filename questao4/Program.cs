80f512501b41cbf9190e9ccb8e85e22022d9f60c
Augusto Kraszczuk <augusto.kraszczuk@gmail.com>
2021-06-29 15:25:12 -0300

    primeiro commit questao 4

---
+++ questao4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input;
+            int input_number;
+            int random_number;
+
+            System.Random random = new System.Random();
+            random_number  = random.Next(11);
+
+            System.Console.WriteLine("Insira um número: ");
+            input = Console.ReadLine();
+            input_number = Convert.ToInt32(input);
+
+            while(input_number != random_number){
+            System.Console.WriteLine("Número errado, tente novamente: ");
+            input = Console.ReadLine();
+            input_number = Convert.ToInt32(input);
+            }
+
+        System.Console.WriteLine("Acertou! Resposta: {0}", random_number);
+        }
+    }
