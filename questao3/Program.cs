dc8acd947e28de0985435ea7099f13e18695154d
Augusto Kraszczuk <61908423+AugustoKras@users.noreply.github.com>
2021-06-29 16:16:43 -0300

    quarto commit

---
+++ questao3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+    interface Imposto
+    {
+    double calculaImposto(double valor);
+    }
+
+    public class ICMS:Imposto
+    {
+    public double calculaImposto(double valor){
+        return valor * 0.3;
+        }
+    }
+
+    public class IPI:Imposto
+    {
+    public double calculaImposto(double valor){
+        if(valor < 25000){
+        return valor * 0.05;
+        }else{
+        return valor * 0.1;
+        }
+        }
+    }
+
+    public class COFINS:Imposto
+    {
+    public double calculaImposto(double valor){
+        if(valor > 17000){
+        return valor * 0.08;
+        }else{
+        return 0;
+        }
+        }
+    }
+
+    public class Program
+    {
+    public static void Main()
+    {
+        System.Console.WriteLine("Insira um valor: ");
+        string input = Console.ReadLine();
+        double numero = Double.Parse(input);
+
+        ICMS icms_class = new ICMS();
+        IPI ipi_class = new IPI();
+        COFINS cofins_class = new COFINS();
+
+        double icms = icms_class.calculaImposto(numero);
+        double ipi = ipi_class.calculaImposto(numero);
+        double cofins = cofins_class.calculaImposto(numero);
+        double total_imposto = icms+ipi+cofins;
+
+        Console.WriteLine("ICMS: {0}", icms);
+        Console.WriteLine("IPI: {0}", ipi);
+        Console.WriteLine("COFINS: {0}", cofins);
+        Console.WriteLine("Total de imposto: {0}", total_imposto);
+    }
+}
